a87bbfe441ca960df2dbac8848161ce0a0d68fed
ameer akech <ameerakech@gmail.com>
2025-04-20 19:07:53 +0300

    added a file

---
+++ Day_1/variables.js
@@ -0,0 +1,17 @@
+//Declaring differnt variables of different types
+let firstName = 'Ameer'
+let lastName ='Akech'
+let country ='Kenya'
+let city = 'Nairobi'
+let age = 20
+let isMarried = true
+
+//Declaring variables with number values
+
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+
+//Variables declared in one line and separated by commas
+
+let name ='Ameer', job = 'teacher', live = 'Kenya'
